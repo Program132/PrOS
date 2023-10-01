b0ad272a806082c8a27fcddd8fdeda4f6d54db54
Program <clement.gauthier132@orange.fr>
2023-10-01 17:10:52 +0200

    Moved all func to cpp file (not defineWindow) + added context menu to create file or a directory (not interaction done)

--- src/Essentials/MainWindow.h
+++ src/Essentials/MainWindow.h
@@ -6,6 +6,7 @@
 #include <QTimer>
 #include <QAbstractAnimation>
 #include <QPropertyAnimation>
+#include <QMenu>
 #include "CustomClass/ImageButton/ImageButton.h"
 #include "CustomClass/TaskbarThemes/TaskBarThemes.h"
 #include "CustomClass/TextLabel/TextLabel.h"
@@ -39,6 +40,14 @@ private:
     [[maybe_unused]] int w;
     [[maybe_unused]] int h;
 
+    void createContextMenu();
+    static void createFolder();
+    static void createFile();
+    void showDesktopContextMenu(const QPoint &pos);
+    void taskBar(QHBoxLayout *layout);
+    void desktop(QVBoxLayout *layout) const;
+    static void setWidgetFrame(QWidget *widget, const QString &startColor, const QString &endColor);
+
     void defineWindow() {
         // Principal Elements :
         auto *mainLayout = new QVBoxLayout;
@@ -62,106 +71,9 @@ private:
         // Calling definition
         desktop(desktopLayout);
         taskBar(taskBarLayout);
-    }
 
-    void taskBar(QHBoxLayout *layout) {
-        // Time + Date Vars:
-        QString dateString = os_getDate();
-        QString timeString = os_getTime();
-
-        // Define taskbar:
-        auto *dateTimeWidget = new QWidget(this);
-        auto *themesWidget = new QWidget(this);
-        auto *appWidget = new QWidget(this);
-
-        setWidgetFrame(dateTimeWidget, "#5B5B5B", "#808080");
-        setWidgetFrame(themesWidget, "#5B5B5B", "#808080");
-        setWidgetFrame(appWidget, "#5B5B5B", "#808080");
-
-        auto *prOSManagerButton = new ImageButton("", projectPath + "/src/img/onoff.png", "background: transparent;", appWidget);
-        auto *terminalButton = new ImageButton("", projectPath + "/src/img/terminal_app.png", "background: transparent;", appWidget);
-
-
-        auto *theme_matrix = new TaskbarThemes("Matrix Theme",
-                                               QString("font-size: 14px; font-weight: bold; background: qlineargradient(x1:0, y1:0, x2:1, y2:0, stop:0 %1, stop:1 %2); border-radius: 5px; ")
-                                               .arg("#099718", "#037A0F"),
-                                               QString("font-size: 13px; font-weight: bold; background: qlineargradient(x1:0, y1:0, x2:1, y2:0, stop:0 %1, stop:1 %2); border-radius: 5px; ")
-                                                       .arg("#099718", "#037A0F"),
-                                               100, 30, themesWidget);
-        auto *theme_purple = new TaskbarThemes("Purple Theme",
-                                               QString("font-size: 14px; font-weight: bold; background: qlineargradient(x1:0, y1:0, x2:1, y2:0, stop:0 %1, stop:1 %2); border-radius: 5px; ")
-                                               .arg("#720997", "#46037A"),
-                                               QString("font-size: 13px; font-weight: bold; background: qlineargradient(x1:0, y1:0, x2:1, y2:0, stop:0 %1, stop:1 %2); border-radius: 5px; ")
-                                                       .arg("#720997", "#46037A"),
-                                               100, 30, themesWidget);
-        auto *theme_blue = new TaskbarThemes("Blue Theme",
-                                             QString("font-size: 14px; font-weight: bold; background: qlineargradient(x1:0, y1:0, x2:1, y2:0, stop:0 %1, stop:1 %2); border-radius: 5px; ")
-                                             .arg("#100CCA", "#4376F9"),
-                                             QString("font-size: 13px; font-weight: bold; background: qlineargradient(x1:0, y1:0, x2:1, y2:0, stop:0 %1, stop:1 %2); border-radius: 5px; ")
-                                                     .arg("#100CCA", "#4376F9"),
-                                             100, 30, themesWidget);
-
-
-        auto *dateText = new TextLabel(dateString,
-                                       "font-size: 16px; color: white; font-weight: bold;",
-                                       100, 30,
-                                       dateTimeWidget);
-        auto *timeText = new TextLabel(timeString,
-                                       "font-size: 16px; color: white; font-weight: bold;",
-                                       100, 30,
-                                       dateTimeWidget);
-
-
-        auto *spacerLeft = new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Preferred);
-        auto *spacerBetweenThemes = new QSpacerItem(10, 0, QSizePolicy::Fixed, QSizePolicy::Preferred);
-        auto *spacerRight = new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Preferred);
-
-        auto *dateTimeLayout = new QHBoxLayout(dateTimeWidget);
-        dateTimeLayout->addWidget(dateText);
-        dateTimeLayout->addWidget(timeText);
-
-        auto *themesLayout = new QHBoxLayout(themesWidget);
-        themesLayout->addWidget(theme_matrix);
-        themesLayout->addItem(spacerBetweenThemes);
-        themesLayout->addWidget(theme_purple);
-        themesLayout->addItem(spacerBetweenThemes);
-        themesLayout->addWidget(theme_blue);
-
-        auto *appLayout = new QHBoxLayout(appWidget);
-        appLayout->addWidget(prOSManagerButton);
-        appLayout->addWidget(terminalButton);
-
-        layout->addWidget(appWidget);
-        layout->addItem(spacerLeft);
-        layout->addWidget(themesWidget);
-        layout->addItem(spacerRight);
-        layout->addWidget(dateTimeWidget);
-
-        // Events :
-
-        auto *timer = new QTimer(this);
-        connect(timer, &QTimer::timeout, [dateText, timeText] {
-            updateTime(dateText, timeText);
-        });
-        timer->start(1000);
-
-        connect(theme_matrix, &QPushButton::clicked, [=, this]() {
-            desktopWidget->setStyleSheet("background-image: url(" + projectPath + "/src/img/matrix_bg.png)");
-        });
-        connect(theme_purple, &QPushButton::clicked, [=, this]() {
-            desktopWidget->setStyleSheet("background-image: url(" + projectPath + "/src/img/purple_bg.png)");
-        });
-        connect(theme_blue, &QPushButton::clicked, [=, this]() {
-            desktopWidget->setStyleSheet("background-image: url(" + projectPath + "/src/img/blue_bg.png)");
-        });
-    }
-
-    void desktop(QVBoxLayout *layout) const {
-        desktopWidget->setStyleSheet("background-image: url(" + projectPath + "/src/img/matrix_bg.png)");
-    }
-
-    static void setWidgetFrame(QWidget *widget, const QString &startColor, const QString &endColor) {
-        widget->setObjectName("WidgetFrame");
-        widget->setStyleSheet(QString("#WidgetFrame { background: qlineargradient(x1:0, y1:0, x2:1, y2:0, stop:0 %1, stop:1 %2); border-radius: 13px; }").arg(startColor, endColor));
+        desktopWidget->setContextMenuPolicy(Qt::CustomContextMenu);
+        connect(desktopWidget, &QWidget::customContextMenuRequested, this, &MainWindow::showDesktopContextMenu);
+        // QWidget::customContextMenuRequested = right click
     }
 };
