07b62d3db166b16ccaede606a0f9fad0758a3fea
Program <clement.gauthier132@orange.fr>
2023-10-01 20:10:53 +0200

    Added min size to app

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     MainWindow PrOS_Window(1000,800);
 
+    PrOS_Window.setMinimumSize(1000,800);
+
     PrOS_Window.show();
     return QApplication::exec();
 }
