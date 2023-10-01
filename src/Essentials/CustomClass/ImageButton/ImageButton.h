ee844da0651341ee90211f4903ded1cf175db31c
Program <clement.gauthier132@orange.fr>
2023-10-01 16:45:31 +0200

    Fixed imagebutton class

--- src/Essentials/CustomClass/ImageButton/ImageButton.h
+++ src/Essentials/CustomClass/ImageButton/ImageButton.h
@@ -1,21 +1,15 @@
 #include <QPushButton>
+#include <QPixmap>
 
 class ImageButton : public QPushButton {
 public:
     ImageButton() = default;
-    ImageButton(const QString& text, const QString& pathImage, QWidget* parent) {
-        QPixmap buttonMapF(pathImage);
-        QPixmap buttonMapS = buttonMapF.scaled(30, 30);
-        QIcon buttonIcon(buttonMapS);
-        this->setIcon(buttonIcon);
-        this->setIconSize(buttonMapS.rect().size());
-        this->setFixedSize(40, 40);
-    }
     ImageButton(const QString& text, const QString& pathImage, const QString& style, QWidget* parent) {
         QPixmap buttonMapF(pathImage);
         QPixmap buttonMapS = buttonMapF.scaled(30, 30);
         QIcon buttonIcon(buttonMapS);
         this->setIcon(buttonIcon);
+        this->setText(text);
         this->setIconSize(buttonMapS.rect().size());
         this->setFixedSize(40, 40);
         this->setStyleSheet(style);
